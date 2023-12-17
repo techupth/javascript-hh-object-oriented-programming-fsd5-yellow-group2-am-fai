d9ab63796b7870581e285233a1a9c30ee9609db5
Am <arphasirisubb@gmail.com>
2023-12-17 21:33:17 +0700

    submit assignment 1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,41 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
+
+const johnnyEmail = new EmailNotification(
+  "1234",
+  new Date(),
+  "Hello Jeno",
+  "Jeno@gmail.com"
+);
+johnnyEmail.send();
+//console.log(johnnyEmail)
+
+const doyoungEmail = new SMSNotification(
+  "2345",
+  new Date(),
+  "Just say hi",
+  "095-845-8705"
+);
+doyoungEmail.send();
+//console.log(doyoungEmail)
